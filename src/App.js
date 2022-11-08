013f094b15f01bb682ddbefc135c6fd86a129f78
tanveerpp <tanveerprogrammerspoint@gmail.com>
2022-11-08 11:52:16 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -7,10 +7,15 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import Card from "./components/Card";
 import Inputs from "./components/Inputs";
 import AddProduct from "./components/AddProduct";
+import Child from "./components/Child";
+var sub=['java,python'];
+var rl=101;
+var fee=25000;
 const App=()=>{
   return(
     <>
-    <AddProduct/>
+    <Child roll={rl} name="ajay" fees={fee} subjects={sub} />
+    <Child/>
     </>
   )
 }
--- src/components/Child.jsx
+++ src/components/Child.jsx
@@ -1,21 +1,26 @@
 import React from "react";
-const Child=()=>{
-    return <h1>Child Component is running</h1>
-}
-const Ppoint=()=>{
-    return(
+import PropTypes from 'prop-types';
+const Child=({roll,name,fees,subjects})=>{
+    return (
         <>
-        <h1>Ppoint is running</h1>
-        <Indore/>
-        <Ujjain/>
+        <h1>Child Component is running</h1>
+        <h2>ROll : {roll}</h2>
+        <h2>NAME : {name}</h2>
+        <h2>FEES : {fees*0.90}</h2>
+        <h2>SUBJECTS : {subjects}</h2>
         </>
     )
 }
-const Ujjain=()=>{
-    return <h1>Ujjain is running</h1>
+Child.propTypes={
+    roll:PropTypes.number,
+    name:PropTypes.string,
+    fees:PropTypes.number,
+    subjects:PropTypes.array
 }
-const Indore=()=>{
-    return <h1>Indore is running</h1>
+Child.defaultProps={
+    roll:1,
+    name:'fullstack',
+    fees:250000,
+    subjects:['frontend','backend','database']
 }
 export default Child;
-export {Ppoint,Ujjain,Indore};
